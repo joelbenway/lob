c5967d8fb92eb52d5baa498b9d34179078ad4cce
Joel Benway <157863269+joelbenway@users.noreply.github.com>
2025-04-21 20:18:12 -0500

    Add test case for remote temperature

--- test/source/lob_env_test.cpp
+++ test/source/lob_env_test.cpp
@@ -328,6 +328,64 @@ TEST_F(LobEnvTestFixture, SolveWithPressureTempHumidity) {
   }
 }
 
+// NOLINTNEXTLINE(readability-function-cognitive-complexity)
+TEST_F(LobEnvTestFixture, SolveWithWeatherStationData) {
+  const float kAltitudeOfFiringSite = 5'280.0F;
+  const float kAirPressure = 30.0F;
+  const float kAltitudeOfBarometer = 0.0F;
+  const float kTemperature = 65.0F;
+  const float kAltitudeOfThermometer = 3'598.0F;
+  const auto kInput = puut->AltitudeOfFiringSiteFt(kAltitudeOfFiringSite)
+                          .AirPressureInHg(kAirPressure)
+                          .AltitudeOfBarometerFt(kAltitudeOfBarometer)
+                          .TemperatureDegF(kTemperature)
+                          .AltitudeOfThermometerFt(kAltitudeOfThermometer)
+                          .Build();
+  constexpr uint16_t kTestStepSize = 100;
+  constexpr uint16_t kVelocityError = 1;
+  constexpr uint16_t kEnergyError = 5;
+  constexpr double kMoaError = 0.1;
+  constexpr double kTimeOfFlightError = 0.01;
+  constexpr size_t kSolutionLength = 12;
+  const std::array<uint32_t, kSolutionLength> kRanges = {
+      0, 150, 300, 600, 900, 1200, 1500, 1800, 2100, 2400, 2700, 3000};
+  const std::vector<lob::Output> kExpected = {
+      {0, 2800, 2696, -1.50F, 0.00F, 0.000F},
+      {150, 2716, 2537, -0.15F, 0.00F, 0.054F},
+      {300, 2634, 2385, 0.02F, 0.00F, 0.110F},
+      {600, 2472, 2102, -3.47F, 0.00F, 0.228F},
+      {900, 2317, 1846, -12.66F, 0.00F, 0.353F},
+      {1200, 2168, 1615, -28.33F, 0.00F, 0.487F},
+      {1500, 2024, 1408, -51.41F, 0.00F, 0.630F},
+      {1800, 1885, 1222, -83.00F, 0.00F, 0.784F},
+      {2100, 1752, 1055, -124.38F, 0.00F, 0.949F},
+      {2400, 1622, 905, -177.12F, 0.00F, 1.127F},
+      {2700, 1497, 770, -243.08F, 0.00F, 1.320F},
+      {3000, 1376, 651, -324.63F, 0.00F, 1.529F}};
+
+  std::array<lob::Output, kSolutionLength> solutions = {};
+  const lob::Options kOptions = {0, 0, lob::NaN(), kTestStepSize};
+  lob::Solve(kInput, &kRanges, &solutions, kOptions);
+  for (size_t i = 0; i < kSolutionLength; i++) {
+    EXPECT_EQ(solutions.at(i).range, kExpected.at(i).range);
+    EXPECT_NEAR(solutions.at(i).velocity, kExpected.at(i).velocity,
+                kVelocityError);
+    EXPECT_NEAR(solutions.at(i).energy, kExpected.at(i).energy, kEnergyError);
+    const double kSolutionElevationMoa =
+        lob::InchToMoa(solutions.at(i).elevation, solutions.at(i).range);
+    const double kExpectedElevationMoa =
+        lob::InchToMoa(kExpected.at(i).elevation, kExpected.at(i).range);
+    EXPECT_NEAR(kSolutionElevationMoa, kExpectedElevationMoa, kMoaError);
+    const double kSolutionDeflectionMoa =
+        lob::InchToMoa(solutions.at(i).deflection, solutions.at(i).range);
+    const double kExpectedDeflectionMoa =
+        lob::InchToMoa(kExpected.at(i).deflection, kExpected.at(i).range);
+    EXPECT_NEAR(kSolutionDeflectionMoa, kExpectedDeflectionMoa, kMoaError);
+    EXPECT_NEAR(solutions.at(i).time_of_flight, kExpected.at(i).time_of_flight,
+                kTimeOfFlightError);
+  }
+}
+
 }  // namespace tests
 
 // This program is free software: you can redistribute it and/or modify
