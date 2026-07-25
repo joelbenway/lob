addeb810465dd26886dcbc7244d8ec8c28cd9734
Joel Benway <157863269+joelbenway@users.noreply.github.com>
2026-07-25 15:41:34 -0500

    fix: fix kDist logic (step_size==0 = distance mode) and clean up unused kT/kX

--- source/lob_solve.cpp
+++ source/lob_solve.cpp
@@ -19,14 +19,14 @@
 namespace lob {
 namespace {
 
-LobOutput LerpOutput(const TrajectoryStateT& s_now, const SecT t_now,
-                     const TrajectoryStateT& s_prev, const SecT t_prev,
+LobOutput LerpOutput(const TrajectoryStateT& s_now,
+                     const TrajectoryStateT& s_prev,
                      double alpha, const LobContext& ctx) {
   const CartesianT<FeetT> kP =
       s_prev.P() + (s_now.P() - s_prev.P()) * FeetT(alpha);
   const CartesianT<FpsT> kV =
       (s_prev.V() + (s_now.V() - s_prev.V()) * FpsT(alpha));
-  const SecT kTimeOfFlight = t_prev + (t_now - t_prev) * SecT(alpha);
+  const SecT kTof = s_prev.TOF() + (s_now.TOF() - s_prev.TOF()) * SecT(alpha);
   const FpsT kVelocity = kV.Magnitude();
   const FtLbsT kEnergy =
       CalculateKineticEnergy(kVelocity, SlugT(LbsT(ctx.mass)));
@@ -37,7 +37,7 @@ LobOutput LerpOutput(const TrajectoryStateT& s_now, const SecT t_now,
   out.energy = kEnergy.U32();
   out.elevation = InchT(kP.Y() - FeetT(ctx.optic_height)).Value();
   out.deflection = InchT(kP.Z()).Value();
-  out.time_of_flight = kTimeOfFlight.Value();
+  out.time_of_flight = kTof.Value();
   return out;
 }
 
@@ -90,22 +90,24 @@ size_t LobSolve(const LobContext* ctx, const uint32_t* pranges,
       CartesianT<FeetT>(FeetT(0.0)),
       CartesianT<FpsT>(FpsT(ctx->velocity) * std::cos(kAngle),
                        FpsT(ctx->velocity) * std::sin(kAngle), FpsT(0.0)));
+  const bool kDist = (ctx->step_size == 0);
   SecT t(0);
+  FeetT x(0);
   size_t index = 0;
 
   spline::CurveView curve(spline::kKnots.data(), &ctx->drags[0]);
 
   while (true) {
     const TrajectoryStateT kS = s;
-    const SecT kT = t;
 
-    SolveStep(&s, &t, &curve, *ctx);
+    if (kDist) SolveStep(&s, &x, &curve, *ctx);
+    else       SolveStep(&s, &t, &curve, *ctx);
 
     if (s.P().X() >= FeetT(pranges[index]) && kS.P().X() < s.P().X()) {
       const double kAlpha =
           ((FeetT(pranges[index]) - kS.P().X()) / (s.P().X() - kS.P().X()))
               .Value();
-      pouts[index] = LerpOutput(s, t, kS, kT, kAlpha, *ctx);
+      pouts[index] = LerpOutput(s, kS, kAlpha, *ctx);
       index++;
     }
 
@@ -113,9 +115,9 @@ size_t LobSolve(const LobContext* ctx, const uint32_t* pranges,
       break;
     }
 
-    if (t >= SecT(ctx->max_time) && kT < SecT(ctx->max_time)) {
-      const double kAlpha = ((SecT(ctx->max_time) - kT) / (t - kT)).Value();
-      pouts[index] = LerpOutput(s, t, kS, kT, kAlpha, *ctx);
+    if (s.TOF() >= SecT(ctx->max_time) && kS.TOF() < SecT(ctx->max_time)) {
+      const double kAlpha = ((SecT(ctx->max_time) - kS.TOF()) / (s.TOF() - kS.TOF())).Value();
+      pouts[index] = LerpOutput(s, kS, kAlpha, *ctx);
       index++;
       break;
     }
@@ -124,14 +126,14 @@ size_t LobSolve(const LobContext* ctx, const uint32_t* pranges,
       const double kAlpha = ((kMinimumSpeed - kS.V().Magnitude()) /
                              (s.V().Magnitude() - kS.V().Magnitude()))
                                 .Value();
-      pouts[index] = LerpOutput(s, t, kS, kT, kAlpha, *ctx);
+      pouts[index] = LerpOutput(s, kS, kAlpha, *ctx);
       index++;
       break;
     }
     // If vertical velocity exceeds 3x horizontal, consider falling straight
     // down.
     if (std::abs(s.V().Y().Value()) > s.V().X().Value() * 3) {
-      pouts[index] = LerpOutput(s, t, kS, kT, 1, *ctx);
+      pouts[index] = LerpOutput(s, kS, 1, *ctx);
       index++;
       break;
     }
