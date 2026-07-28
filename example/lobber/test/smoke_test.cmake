5601c2e1186470460a42c006d4d6aed115c587e3
Joel Benway <157863269+joelbenway@users.noreply.github.com>
2026-07-27 20:11:58 -0500

    Normalize SPDX-License-Identifier headers and add GPL boilerplate

--- example/lobber/test/smoke_test.cmake
+++ example/lobber/test/smoke_test.cmake
@@ -51,3 +51,17 @@ foreach(FIELD range velocity energy elevation deflection time_of_flight)
   endif()
 endforeach()
 message(STATUS "PASS: --json")
+
+# This file is part of lob.
+#
+# lob is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# lob is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# lob. If not, see <https://www.gnu.org/licenses/>.
