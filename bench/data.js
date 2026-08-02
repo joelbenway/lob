a35c8af3497290a6b3933d094c1216a6b1411035
github-action-benchmark <github@users.noreply.github.com>
2026-08-02 16:41:32 +0000

    add lob instruction counts (customSmallerIsBetter) benchmark result for 9183fe4c6cc33e740031a17fbb8af81ffa461438

---
+++ bench/data.js
@@ -0,0 +1,67 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1785688892942,
+  "repoUrl": "https://github.com/joelbenway/lob",
+  "entries": {
+    "lob instruction counts": [
+      {
+        "commit": {
+          "author": {
+            "email": "157863269+joelbenway@users.noreply.github.com",
+            "name": "Joel Benway",
+            "username": "joelbenway"
+          },
+          "committer": {
+            "email": "157863269+joelbenway@users.noreply.github.com",
+            "name": "Joel Benway",
+            "username": "joelbenway"
+          },
+          "distinct": true,
+          "id": "9183fe4c6cc33e740031a17fbb8af81ffa461438",
+          "message": "ci: prevent collision between track and docs",
+          "timestamp": "2026-08-02T11:29:27-05:00",
+          "tree_id": "80fd70ff39624e9aeb3473fd0d67b13238c705a0",
+          "url": "https://github.com/joelbenway/lob/commit/9183fe4c6cc33e740031a17fbb8af81ffa461438"
+        },
+        "date": 1785688892041,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "lob_bench/build_basic",
+            "value": 840,
+            "unit": "Ir/op"
+          },
+          {
+            "name": "lob_bench/build_full",
+            "value": 1994,
+            "unit": "Ir/op"
+          },
+          {
+            "name": "lob_bench/build_custom_table",
+            "value": 7652.001,
+            "unit": "Ir/op"
+          },
+          {
+            "name": "lob_bench/build_boatright",
+            "value": 585563.006,
+            "unit": "Ir/op"
+          },
+          {
+            "name": "lob_bench/build_zero_search",
+            "value": 79386.005,
+            "unit": "Ir/op"
+          },
+          {
+            "name": "lob_bench/solve_basic",
+            "value": 261181.995,
+            "unit": "Ir/op"
+          },
+          {
+            "name": "lob_bench/solve_boatright",
+            "value": 261339.01,
+            "unit": "Ir/op"
+          }
+        ]
+      }
+    ]
+  }
+}
